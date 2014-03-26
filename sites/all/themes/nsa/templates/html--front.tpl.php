01a88024b464f215bb3ff8868ba4ad59cf9aacd7
Luis Mori <luismori@glr.pe>
2014-03-26 17:29:13 -0500

    proyecto nuestra palabra

---
+++ sites/all/themes/nsa/templates/html--front.tpl.php
@@ -0,0 +1,24 @@
+<?php print $doctype; ?>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
+<head<?php print $rdf->profile; ?>>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>  
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <!--Adobe Edge Runtime-->
+    <script type="text/javascript" charset="utf-8" src="Untitled-1_edgePreload.js"></script>
+    <style>
+        .edgeLoad-EDGE-1245282 { visibility:hidden; }
+    </style>
+<!--Adobe Edge Runtime End-->
+</head>
+<body<?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html> 
